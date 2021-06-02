5e81c47b422a7f99df5cac83cfe5f48b0f4c7c81
gitrandreza <85165948+gitrandreza@users.noreply.github.com>
2021-06-02 12:02:42 -0500

    Test Connection Begin Customer View Design

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -29,23 +29,165 @@ namespace SU21_Final_Project
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabItemCategory = new System.Windows.Forms.TabControl();
+            this.tabClothes = new System.Windows.Forms.TabPage();
+            this.tabGifts = new System.Windows.Forms.TabPage();
+            this.tabBags = new System.Windows.Forms.TabPage();
+            this.tabAll = new System.Windows.Forms.TabPage();
+            this.menuStrip1.SuspendLayout();
+            this.tabItemCategory.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMessage.Location = new System.Drawing.Point(720, 377);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(95, 15);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "labelTestMessage";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(827, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newOrderToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newOrderToolStripMenuItem
+            // 
+            this.newOrderToolStripMenuItem.Name = "newOrderToolStripMenuItem";
+            this.newOrderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newOrderToolStripMenuItem.Text = "&New Order";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "&Close";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // tabItemCategory
+            // 
+            this.tabItemCategory.Controls.Add(this.tabClothes);
+            this.tabItemCategory.Controls.Add(this.tabGifts);
+            this.tabItemCategory.Controls.Add(this.tabBags);
+            this.tabItemCategory.Controls.Add(this.tabAll);
+            this.tabItemCategory.Location = new System.Drawing.Point(0, 28);
+            this.tabItemCategory.Name = "tabItemCategory";
+            this.tabItemCategory.SelectedIndex = 0;
+            this.tabItemCategory.Size = new System.Drawing.Size(421, 370);
+            this.tabItemCategory.TabIndex = 2;
+            // 
+            // tabClothes
+            // 
+            this.tabClothes.Font = new System.Drawing.Font("Arial Unicode MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabClothes.Location = new System.Drawing.Point(4, 22);
+            this.tabClothes.Name = "tabClothes";
+            this.tabClothes.Padding = new System.Windows.Forms.Padding(3);
+            this.tabClothes.Size = new System.Drawing.Size(413, 344);
+            this.tabClothes.TabIndex = 0;
+            this.tabClothes.Text = "           Clothes         ";
+            this.tabClothes.UseVisualStyleBackColor = true;
+            // 
+            // tabGifts
+            // 
+            this.tabGifts.Location = new System.Drawing.Point(4, 22);
+            this.tabGifts.Name = "tabGifts";
+            this.tabGifts.Padding = new System.Windows.Forms.Padding(3);
+            this.tabGifts.Size = new System.Drawing.Size(413, 344);
+            this.tabGifts.TabIndex = 1;
+            this.tabGifts.Text = "              Gifts             ";
+            this.tabGifts.UseVisualStyleBackColor = true;
+            // 
+            // tabBags
+            // 
+            this.tabBags.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabBags.Location = new System.Drawing.Point(4, 22);
+            this.tabBags.Name = "tabBags";
+            this.tabBags.Size = new System.Drawing.Size(413, 344);
+            this.tabBags.TabIndex = 2;
+            this.tabBags.Text = "            Bags           ";
+            this.tabBags.UseVisualStyleBackColor = true;
+            // 
+            // tabAll
+            // 
+            this.tabAll.Location = new System.Drawing.Point(4, 22);
+            this.tabAll.Name = "tabAll";
+            this.tabAll.Size = new System.Drawing.Size(413, 344);
+            this.tabAll.TabIndex = 3;
+            this.tabAll.Text = "            All           ";
+            this.tabAll.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 332);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(827, 401);
+            this.Controls.Add(this.tabItemCategory);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Tag = "2021.05.02";
             this.Text = "Imprint Manager System";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabItemCategory.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newOrderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabItemCategory;
+        private System.Windows.Forms.TabPage tabClothes;
+        private System.Windows.Forms.TabPage tabGifts;
+        private System.Windows.Forms.TabPage tabBags;
+        private System.Windows.Forms.TabPage tabAll;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -5,7 +5,7 @@
 // Course: INEW 2332.7Z1 (Final Project)
 // Program Description: A sale management application of custom gift service that print design on items 
 //*******************************************
-// Form Purpose: This is the point of sale form or the customer view
+// Form Purpose: This is the point of sale view or the customer view
 //*******************************************
 //*******************************************
 
@@ -18,12 +18,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 
 namespace SU21_Final_Project
 {
     public partial class frmMain : Form
     {
+        //Establish connection to the database       
+        private SqlConnection dataConnection;
+        string query;
+
+        //connect to database
+        
+
         public frmMain()
         {
             InitializeComponent();
@@ -31,6 +39,29 @@ namespace SU21_Final_Project
 
         private void frmMain_Load(object sender, EventArgs e)
         {
+            try
+            {
+                //connect to database
+                dataConnection = new SqlConnection("Server=cstnt.tstc.edu;" +
+                    "Database= inew2332su21 ;User Id=RandrezaVoharisoaM21Su2332; password = 1760945");
+                //open the database
+                dataConnection.Open();
+                //display message
+                lblMessage.Text = dataConnection.State.ToString();
+                //close the database
+                dataConnection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error :" + ex);
+            }
+           
+
+            //test query
+            SqlCommand Cmd;
+            SqlDataAdapter dataAd;
+            DataTable dt;
 
         }
     }
