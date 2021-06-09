d2ffefe7ca1a7db395adf2145e1a097e473aca36
Ricky <mrandrezavoha@mymail.tstc.edu>
2021-06-09 15:34:05 -0500

    Project Report and GitHub Status #1

--- Login.cs
+++ Login.cs
@@ -13,26 +13,52 @@ namespace SU21_Final_Project
 {
     public partial class frmLogin : Form
     {
-        public SqlConnection Connection;
+        private SqlConnection Connection;
+        private SqlCommand command; //command object
+        private SqlDataReader reader; //datareader object
+        public frmMain fmain;
         public frmLogin()
         {
             InitializeComponent();
         }
+        public frmLogin(frmMain formMain)
+        {
+            fmain = formMain;
+        }
+
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
             try
-            {
-                Connection.Open();
-                //connect to database
+            { //connect to database
+               
+               
                 Connection = new SqlConnection("Server=cstnt.tstc.edu;" +
                     "Database= inew2332su21 ;User Id=RandrezaVoharisoaM21Su2332; password = 1760945");
-               //check for password and username
 
-               
-               
-                 lblMessage.Text = Connection.State.ToString();
-              
+                Connection.Open();
+                //check for password and username
+                command = new SqlCommand("SELECT Username, Password FROM RandrezaVoharisoaM21Su2332.Users;", Connection);
+
+                //gets the results from the sql command
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    //iterates through the username column to find a matching value
+                    //after it finds a matching user name it then moves to the password column for that row.
+                    if (reader["Username"].ToString() == tbxUsername.Text) //compares curent username on the current row
+                    {
+                        if (reader["Password"].ToString() == tbxPassword.Text ) //compares the password of the current row
+                        {
+                            lblMessage.Text = Connection.State.ToString();
+                           
+                           
+                          
+                        }
+                    }
+                }
+             
                 Connection.Close();
 
             }
